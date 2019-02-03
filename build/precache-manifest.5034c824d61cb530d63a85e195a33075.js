94248a12502ca624674d0e6e06dd13031bd23599
Hugh A. Miles <hmiles23@ufl.edu>
2019-02-03 13:00:39 -0800

    this should work now

---
+++ build/precache-manifest.5034c824d61cb530d63a85e195a33075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c7d4c0b2b93e2bf9b39",
+    "url": "/static/js/main.9c7d4c0b.chunk.js"
+  },
+  {
+    "revision": "dac5616ad25d4a2ba8ac",
+    "url": "/static/js/1.dac5616a.chunk.js"
+  },
+  {
+    "revision": "9c7d4c0b2b93e2bf9b39",
+    "url": "/static/css/main.d4ba7665.chunk.css"
+  },
+  {
+    "revision": "42da429bd60033af90932bb088076c47",
+    "url": "/index.html"
+  }
+];
